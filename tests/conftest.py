4c66b61579d35373b6b5d14e47fbae7b7ba2f25f
Pinqvi <black.angel.kab@gmail.com>
2026-05-23 17:23:04 +0300

    Фикстуры Pytest

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+import pytest  # Импортируем pytest
+from playwright.sync_api import sync_playwright, \
+    Page, Playwright  # Имопртируем класс страницы, будем использовать его для аннотации типов
+
+
+@pytest.fixture
+def chromium_page(playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    yield browser.new_page()
+    browser.close()
