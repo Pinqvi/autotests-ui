b3aec73fd6019c73886d784059863558a816adf9
Pinqvi <black.angel.kab@gmail.com>
2026-05-30 20:13:57 +0300

    Наращивание тестовой базы и рефакторинг

--- pages/authentication/login_page.py
+++ pages/authentication/login_page.py
@@ -1,9 +1,10 @@
 from components.authentication.login_form_component import LoginFormComponent
 from pages.base_page import BasePage
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 from elements.button import Button
 from elements.link import Link
 from elements.text import Text
+import re
 
 
 class LoginPage(BasePage):
@@ -22,6 +23,7 @@ class LoginPage(BasePage):
 
     def click_registration_link(self):
         self.registration_link.click()
+        self.check_current_url(re.compile(".*/#/auth/registration"))
 
     def check_visible_wrong_email_or_password_alert(self):
         self.wrong_email_or_password_alert.check_visible()
--- pages/authentication/registration_page.py
+++ pages/authentication/registration_page.py
@@ -2,6 +2,8 @@ from components.authentication.registration_form_component import RegistrationFo
 from pages.base_page import BasePage
 from elements.button import Button
 from playwright.sync_api import Page
+from elements.link import Link
+import re
 
 
 class RegistrationPage(BasePage):
@@ -9,8 +11,13 @@ class RegistrationPage(BasePage):
         super().__init__(page)
 
         self.registration_button = Button(page, 'registration-page-registration-button', 'Registration')
+        self.login_link = Link(page, 'registration-page-login-link', 'Login')
 
         self.registration_form = RegistrationFormComponent(page)
 
     def click_registration_button(self):
         self.registration_button.click()
+
+    def click_login_link(self):
+        self.login_link.click()
+        self.check_current_url(re.compile(".*/#/auth/login"))
